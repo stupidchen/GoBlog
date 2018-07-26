edae1fae9531e93e74bf96e6906e76ad0d7a6859
Mike Chen <stupidchen@foxmail.com>
2018-07-27 00:08:38 +0800

    Finished: Model interface and GET/POST/PUT/DELETE of comment.

--- model/comment.go
+++ model/comment.go
@@ -2,11 +2,51 @@ package model
 
 import (
 	"github.com/jinzhu/gorm"
+	"blog/global"
+	"time"
 )
 
 type Comment struct {
 	Object `json:"object,omitempty"`
 	gorm.Model
+	Article int `gorm:"not null"`
 	Author string `gorm:"not null"`
 	Content string `gorm:"type:text"`
+}
+
+func FindCommentById(id uint) *Comment {
+	db := global.GLOBAL.DB
+	var c Comment
+	if db.Find(&c, id).RecordNotFound() {
+		return nil
+	}
+	return &c
+}
+
+func FindCommentByArticleId(articleId uint) *[]Comment {
+	var comments []Comment
+	db := global.GLOBAL.DB
+	db.Where("article = ?", articleId).Find(&comments)
+	return &comments
+}
+
+func AddComment(comment Comment) error {
+	db := global.GLOBAL.DB
+	return db.Create(&comment).Error
+}
+
+func UpdateComment(comment Comment) error {
+	db := global.GLOBAL.DB
+	return db.Model(&comment).Updates(comment).Error
+}
+
+func DeleteComment(id uint) error {
+	db := global.GLOBAL.DB
+	a := Comment{Model: gorm.Model{
+		ID:        id,
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+		DeletedAt: nil,
+	}}
+	return db.Delete(&a).Error
 }
--- view/comment.go
+++ view/comment.go
@@ -1,24 +1,105 @@
 package view
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"blog/model"
+	"fmt"
+)
 
 type CommentHandler struct {
 }
 
 func (h *CommentHandler) Get (r *http.Request) *ResponseData {
+	s2 := getSubPath(r.URL.Path, 2)
+	if s2 != nil && len(*s2) != 0 {
+		id, err := strconv.ParseUint(*s2, 10, 64)
+		if err != nil {
+			return InitError(err.Error())
+		}
+		a := model.FindCommentById(uint(id))
+		if a == nil {
+			return InitError(fmt.Sprintf("Comment %d does not exist.", id))
+		}
+		return &ResponseData{
+			Object:    *a,
+			Ok:        true,
+			ModelType: "Comment",
+			Message:   "Get comment success.",
+		}
+
+	} else {
+		a := model.GetAllArticles()
+		return &ResponseData{
+			Object:    *a,
+			Ok:        true,
+			ModelType: "Comments",
+			Message:   "Get comments success.",
+		}
+	}
 	return nil
 }
 
 func (h *CommentHandler) Post (r *http.Request, body *RequestData) *ResponseData {
-	return nil
+	s2 := getSubPath(r.URL.Path, 2)
+	if s2 != nil && len(*s2) != 0 {
+		id, err := strconv.ParseInt(*s2, 10, 64)
+		if err != nil {
+			return InitError(err.Error())
+		}
+		body.Article.ID = uint(id)
+		err = model.AddComment(body.Comment)
+		if err != nil {
+			return InitError(fmt.Sprintf("Cannot create comment %d due to %s.", id, err.Error()))
+		} else {
+			return InitHint(fmt.Sprintf("Comment(id: %d) was created.", body.Comment.ID))
+		}
+	} else {
+		err := model.AddComment(body.Comment)
+		if err != nil {
+			return InitError(fmt.Sprintf("Cannot create comment due to %s.", err.Error()))
+		} else {
+			return InitHint(fmt.Sprintf("Comment(id: %d) was created.", body.Comment.ID))
+		}
+	}
 }
 
 func (h *CommentHandler) Put (r *http.Request, body *RequestData) *ResponseData {
-	return nil
+	s2 := getSubPath(r.URL.Path, 2)
+	if s2 != nil && len(*s2) != 0 {
+		id, err := strconv.ParseInt(*s2, 10, 64)
+		if err != nil {
+			return InitError(err.Error())
+		}
+		body.Comment.ID = uint(id)
+		err = model.UpdateComment(body.Comment)
+		if err != nil {
+			return InitError(fmt.Sprintf("Cannot update comment %d due to %s.", id, err.Error()))
+		}
+	} else {
+		err := model.UpdateComment(body.Comment)
+		if err != nil {
+			return InitError(fmt.Sprintf("Cannot update comment due to %s.", err.Error()))
+		}
+	}
+	return InitHint(fmt.Sprintf("Comment(id: %d) was updated.", body.Comment.ID))
 }
 
 func (h *CommentHandler) Delete (r *http.Request) *ResponseData {
-	return nil
+	s2 := getSubPath(r.URL.Path, 2)
+	if s2 != nil && len(*s2) != 0 {
+		id, err := strconv.ParseInt(*s2, 10, 64)
+		if err != nil {
+			return InitError(err.Error())
+		}
+		err = model.DeleteComment(uint(id))
+		if err != nil {
+			return InitError(fmt.Sprintf("Cannot delete comment(id: %d) due to %s.", id, err.Error()))
+		}
+		return InitHint(fmt.Sprintf("Comment(id: %d) was deleted.", id))
+	} else {
+		return InitError("Invalid parameter.")
+	}
 }
 
 func init() {
